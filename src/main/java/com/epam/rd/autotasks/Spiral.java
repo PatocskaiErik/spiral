56442935813203765b4ad8441acce364ba037a9f
Erik <patoerik93@gmail.com>
2023-06-22 18:01:31 +0200

    Added calculations

--- src/main/java/com/epam/rd/autotasks/Spiral.java
+++ src/main/java/com/epam/rd/autotasks/Spiral.java
@@ -2,6 +2,39 @@ package com.epam.rd.autotasks;
 
 class Spiral {
     static int[][] spiral(int rows, int columns) {
-        return null;
+        int[][] matrix = new int[rows][columns];
+
+        int num = 1;
+        int topRow = 0;
+        int bottomRow = rows - 1;
+        int leftColumn = 0;
+        int rightColumn = columns - 1;
+
+        while(topRow <= bottomRow && leftColumn <= rightColumn) {
+            for(int i = leftColumn; i <= rightColumn; i++) {
+                matrix[topRow][i] = num++;
+            }
+            topRow++;
+
+            for(int i = topRow; i<= bottomRow; i++) {
+                matrix[i][rightColumn] = num++;
+            }
+            rightColumn--;
+
+            if(topRow <= bottomRow) {
+                for(int i = rightColumn; i >= leftColumn; i--) {
+                    matrix[bottomRow][i] = num++;
+                }
+                bottomRow--;
+            }
+
+            if(leftColumn <= rightColumn) {
+                for(int i = bottomRow; i >= topRow; i--) {
+                    matrix[i][leftColumn] = num++;
+                }
+                leftColumn++;
+            }
+        }
+        return matrix;
     }
 }
